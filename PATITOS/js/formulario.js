1f471e623d3aaff01ab4aff02b3f61eaf1b69acd
Maria Emilia Quiroga <103537790+emiliaquiroga@users.noreply.github.com>
2022-06-30 21:52:43 -0300

    Add files via upload

---
+++ PATITOS/js/formulario.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("formulario").addEventListener('submit', validarFormulario); 
+  let formulario = document.getElementById("formulario");
+  formulario.addEventListener('submit', function() {
+    formulario.reset();
+});
+
+function validarFormulario(evento) {
+  evento.preventDefault();
+  var usuario = document.getElementById('usuario').value;
+  if(usuario.length == 0) {
+    alert('No has escrito tu nombre');
+    return;
+  }
+  evento.preventDefault();
+  var usuario = document.getElementById('email').value;
+  if(usuario.length == 0) {
+        alert('Ingresa un correo electrónico válido!');
+        return;
+      }
+
+  var clave = document.getElementById('clave').value;
+  if (clave.length < 6) {
+    alert('La clave no es válida');
+    return;
+  }
+  this.submit();
+}
+
+});
